edc71766ca3942ffeb8de65d8a7d63b1dccf44d1
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-06-26 08:30:13 +0900

    Four Panel Modification

--- src/panels/four_panel.py
+++ src/panels/four_panel.py
@@ -4,10 +4,11 @@ from src.panel import Panel
 
 
 class FourPanel(Panel):
-    def __init__(self, width=800, height=480, settings={}):
-        super().__init__(width, height, settings)
-        self.border_color = settings.get("border_color", "black")
-        self.border_width = settings.get("border_width", 2)
+    def __init__(self, width=800, height=480, settings={}, DEBUG=False):
+        super().__init__(width, height, settings, DEBUG)
+
+        # Margin, padding and border settings
+        self.padding = settings.get("padding", 10)
 
     def set_panels(self, panel1: Panel, panel2: Panel, panel3: Panel, panel4: Panel):
         self.panel1 = panel1
@@ -17,53 +18,94 @@ class FourPanel(Panel):
 
     def get_panel_size(self):
         return (
-            (self.width - self.padding * 2) // 2,
-            (self.height - self.padding * 2) // 2,
+            (self.width - self.margin * 2) // 2 - self.padding * 2,
+            (self.height - self.margin * 2) // 2 - self.padding * 2,
         )
 
     def draw(self):
-        image = Image.new("RGB", (self.width, self.height), "white")
+        image = super().draw()
         draw = ImageDraw.Draw(image)
 
+        spacing = self.margin + self.padding
         panel_width, panel_height = self.get_panel_size()
 
         panel1_image = self.panel1.draw()
-        image.paste(panel1_image, (self.padding, self.padding))
+        image.paste(panel1_image, (spacing, spacing))
 
         panel2_image = self.panel2.draw()
-        image.paste(panel2_image, (self.padding + panel_width, self.padding))
+        image.paste(panel2_image, (self.width // 2 + self.padding, spacing))
 
         panel3_image = self.panel3.draw()
-        image.paste(panel3_image, (self.padding, self.padding + panel_height))
+        image.paste(panel3_image, (spacing, self.height // 2 + self.padding))
 
         panel4_image = self.panel4.draw()
         image.paste(
-            panel4_image, (self.padding + panel_width, self.padding + panel_height)
+            panel4_image,
+            (self.width // 2 + self.padding, self.height // 2 + self.padding),
         )
 
-        draw.rectangle(
-            [
-                (self.padding, self.padding),
-                (self.width - self.padding, self.height - self.padding),
-            ],
-            outline=self.border_color,
-            width=self.border_width,
-        )
         draw.line(
             [
-                (self.padding + panel_width, self.padding),
-                (self.padding + panel_width, self.height - self.padding),
+                (self.width // 2, self.margin),
+                (self.width // 2, self.height - self.margin),
             ],
             fill=self.border_color,
             width=self.border_width,
         )
+
         draw.line(
             [
-                (self.padding, self.padding + panel_height),
-                (self.width - self.padding, self.padding + panel_height),
+                (self.margin, self.height // 2),
+                (self.width - self.margin, self.height // 2),
             ],
             fill=self.border_color,
             width=self.border_width,
         )
+
+        if self.DEBUG:
+            draw.rectangle(
+                [
+                    (spacing, spacing),
+                    (spacing + panel_width, spacing + panel_height),
+                ],
+                outline="blue",
+                width=2,
+            )
+
+            draw.rectangle(
+                [
+                    (self.width // 2 + self.padding, spacing),
+                    (
+                        self.width // 2 + self.padding + panel_width,
+                        spacing + panel_height,
+                    ),
+                ],
+                outline="blue",
+                width=2,
+            )
+
+            draw.rectangle(
+                [
+                    (spacing, self.height // 2 + self.padding),
+                    (
+                        spacing + panel_width,
+                        self.height // 2 + self.padding + panel_height,
+                    ),
+                ],
+                outline="blue",
+                width=2,
+            )
+
+            draw.rectangle(
+                [
+                    (self.width // 2 + self.padding, self.height // 2 + self.padding),
+                    (
+                        self.width // 2 + self.padding + panel_width,
+                        self.height // 2 + self.padding + panel_height,
+                    ),
+                ],
+                outline="blue",
+                width=2,
+            )
 
         return image
