d1b5f4d9180eca3ad657fe26305139985af66b69
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-06-25 10:04:49 +0900

    Margin Modification

--- src/panel.py
+++ src/panel.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 
 class Panel:
@@ -6,9 +6,23 @@ class Panel:
         self.width = width
         self.height = height
         self.settings = settings
-        self.padding = settings.get("padding", 10)
+
+        # Margin, padding and border settings
+        self.margin = settings.get("margin", 10)
+        self.border_color = settings.get("border_color", "black")
+        self.border_width = settings.get("border_width", 2)
 
     def draw(self):
         image = Image.new("RGB", (self.width, self.height), "white")
+
+        draw = ImageDraw.Draw(image)
+        draw.rectangle(
+            [
+                (self.margin, self.margin),
+                (self.width - self.margin, self.height - self.margin),
+            ],
+            outline=self.border_color,
+            width=self.border_width,
+        )
 
         return image
