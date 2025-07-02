91271c3a083691b4eed940186d93b1beea3c5056
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-07-02 09:29:58 +0900

    Get Data

--- src/panels/toggl_panel.py
+++ src/panels/toggl_panel.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 from src.panel import Panel
 import src.helper as Helper
 from src.palette import *
+from datetime import datetime, timezone
 
 import src.api.toggl as TogglAPI
 
@@ -13,20 +14,45 @@ class TogglPanel(Panel):
 
         # Toggl API settings
         self.auth = f"{settings.get('api_key', '')}:api_token"
-        self.api_key_status = TogglAPI._verify_api_key(self.auth)
+        self.api_key_status = TogglAPI.verify_api_key(self.auth)
 
         # Margin, padding and border settings
         self.padding = settings.get("padding", 10)
+
+        # Toggl Data
+        self.projects = TogglAPI.get_workspace_projects(self.auth, self.api_key_status)
 
     def _draw(self, image):
         if not self.api_key_status:
             image = self._draw_api_invalid(image)
             return image
 
-        current_entry = TogglAPI._get_current_time_entry(self.auth)
-        print(f"Current entry: {current_entry}")
+        current_entry = TogglAPI.get_current_time_entry(self.auth)
+        current_project = self._get_project_details(
+            current_entry.get("project_id"), current_entry.get("workspace_id")
+        )
+        print(
+            f"{current_entry.get('description')}·{current_project.get('name')}/{current_project.get('color')}"
+        )
+        start_time = datetime.fromisoformat(current_entry.get("start"))
+        local_tz = datetime.now().astimezone().tzinfo
+        start_time_local = start_time.astimezone(local_tz)
+        formatted_time = start_time_local.strftime("%Y-%m-%d %H:%M:%S")
+
+        print(f"{formatted_time}")
 
         return image
+
+    def _get_project_details(self, project_id, workspace_id=None):
+        if not project_id:
+            return None
+        if project_id in self.projects:
+            return self.projects[project_id]
+
+        if workspace_id is None:
+            return None
+        self.projects = TogglAPI.get_workspace_projects(self.auth, workspace_id)
+        return self.projects.get(project_id, None)
 
     def _draw_api_invalid(self, image):
         draw = ImageDraw.Draw(image)
