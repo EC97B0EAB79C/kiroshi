20a5daf6d2e31087d1ea8087ea7bddfc6f6eea62
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-07-11 10:08:12 +0900

    Implement Main Test Script

---
+++ main_test.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+from PIL import Image, ImageDraw, ImageFont
+
+from src.panel import Panel
+from src.panels.text_panel import TextPanel
+from src.panels.time_panel import TimePanel
+from src.panels.horizontal_panel import HorizontalPanel
+from src.panels.vertical_panel import VerticalPanel
+from src.panels.four_panel import FourPanel
+from src.panels.picture_panel import PicturePanel
+from src.panels.toggl_panel import TogglPanel
+
+DEBUG = False
+
+
+def test_four_panel():
+    panel = FourPanel(
+        width=800,
+        height=480,
+        settings={
+            "padding": 0,
+        },
+        DEBUG=DEBUG,
+    )
+    size = panel.get_panel_size()
+
+    text_panel = TextPanel(
+        width=size[0],
+        height=size[1],
+        settings={
+            "text": "Text Panel",
+            "font": "fonts/roboto_mono/static/RobotoMono-Regular.ttf",
+            "font_size": 24,
+            "font_color": "black",
+            "align": "center",
+            "margin": 10,
+            "border_width": 0,
+            "padding": 10,
+        },
+        DEBUG=DEBUG,
+    )
+
+    time_panel = TimePanel(
+        width=size[0],
+        height=size[1],
+        settings={
+            "font": "fonts/roboto_mono/static/RobotoMono-Regular.ttf",
+            "font_size": 24,
+            "font_color": "black",
+            "align": "center",
+            "margin": 10,
+            "padding": 10,
+        },
+        DEBUG=DEBUG,
+    )
+
+    toggl_panel = TogglPanel(
+        width=size[0],
+        height=size[1],
+        settings={
+            "api_key": "",
+            "margin": 0,
+            "padding": 10,
+            "border_color": "black",
+            "border_width": 0,
+            "font": "fonts/noto_sans_with_emoji/NotoSansWithEmoji-Scaled.ttf",
+        },
+        DEBUG=DEBUG,
+    )
+
+    panel.set_panels(text_panel, time_panel, toggl_panel, time_panel)
+    image = panel.draw()
+
+    return image
+
+
+from waveshare_epd import epd7in3e
+
+if __name__ == "__main__":
+    image = test_four_panel()
+
+    epd = epd7in3e.EPD()
+    epd.init()
+    epd.Clear()
+
+    # Display the image on the e-Paper
+    epd.display(epd.getbuffer(image))
+    epd.sleep()
