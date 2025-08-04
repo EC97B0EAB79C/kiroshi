1a88b0a00d5f4e7be044aedb5e9014f35a025bd4
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-08-04 10:58:47 +0900

    Setting Class

---
+++ src/setting.py
@@ -0,0 +1,34 @@
+from src.helper import load_json
+
+
+class Setting:
+    def __init__(self, settings_file="example/setting.json"):
+        self.settings_file = settings_file
+        self.settings = load_json(self.settings_file)
+        if not self.settings:
+            raise ValueError(f"Failed to load settings from {self.settings_file}")
+
+        self.panels = load_json(self.settings.get("panel_spec", "example/panels.json"))
+        if not self.panels:
+            raise ValueError(
+                f"Failed to load panels from {self.settings.get('panel_spec', 'example/panels.json')}"
+            )
+
+        self.schedule = self.settings.get("schedule", [])
+        if not self.schedule:
+            raise ValueError("No schedule found in settings")
+
+        self.schedule_length = len(self.schedule)
+        if self.schedule_length == 0:
+            raise ValueError("Schedule is empty")
+
+        #
+        self.current_panel_index = 0
+
+    def get_next_panel(self):
+        current_panel_spec = self.schedule[self.current_panel_index]
+        panel_id = current_panel_spec.get("id", 0)
+        panel_duration = current_panel_spec.get("duration", 5)
+
+        self.current_panel_index = (self.current_panel_index + 1) % self.schedule_length
+        return self.panels[panel_id], panel_duration
