7b7b246c79521ba46c10228354bad8975b172c93
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-08-13 10:01:38 +0900

    Install Script

--- install.sh
+++ install.sh
@@ -7,29 +7,101 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-# Parameters
+# --- Parameters ---
+# Project parameters
 GIT_REPO_URL="https://github.com/EC97B0EAB79C/kiroshi"
 PYTHON_SCRIPT="main.py"
 SERVICE_NAME="kiroshi"
-RUN_USER="pi"
 
-# Pre-check
+# Path
+USER_HOME=$(eval echo "~$USER")
+LOCAL_BIN="$USER_HOME/.local/bin"
+INSTALL_DIR="$LOCAL_BIN/$SERVICE_NAME"
+SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
+WRAPPER_SCRIPT_PATH_START="$INSTALL_DIR/start.sh"
+WRAPPER_SCRIPT_PATH_STOP="$INSTALL_DIR/stop.sh"
+
+# --- Pre-check ---
+# Check if the script is run with sudo privileges
 if [ "$EUID" -ne 0 ]; then
   echo -e "${YELLOW}Please run this script with sudo privileges.${NC}"
   echo "Usage: sudo ./setup_service.sh"
   exit 1
 fi
 
-# Clone repository
+# Check if python3 is installed
+if ! command -v python3 &> /dev/null; then
+  echo -e "${YELLOW}Python 3 is not installed. Please install it and try again.${NC}"
+  exit 1
+fi
+
+# --- Clone repository ---
+mkdir -p "$INSTALL_DIR"
+git clone "$GIT_REPO_URL" "$INSTALL_DIR"
 
 
-# Install dependencies
+# --- Install dependencies ---
+pip install -r "$INSTALL_DIR/requirements.txt"
 
 
 # Create wrapper script
+cat <<EOF > "$WRAPPER_SCRIPT_PATH_START"
+#!/bin/bash
+set -e
 
+cd "$INSTALL_DIR" || exit
+
+git checkout main
+git pull
+pip install -r "$INSTALL_DIR/requirements.txt"
+
+python3 "$INSTALL_DIR/$PYTHON_SCRIPT"
+EOF
+
+chmod +x "$WRAPPER_SCRIPT_PATH_START"
+chown "$USER:$USER" "$WRAPPER_SCRIPT_PATH_START"
+
+
+# cat <<EOF > "$WRAPPER_SCRIPT_PATH_STOP"
+# #!/bin/bash
+
+# set -e
+
+# cd "$INSTALL_DIR" || exit
+
+# git checkout main
+# git pull
+# pip install -r "$INSTALL_DIR/requirements.txt"
+
+# pkill -f "$INSTALL_DIR/$PYTHON_SCRIPT"
+# EOF
+
+# chmod +x "$WRAPPER_SCRIPT_PATH_STOP"
+# chown "$USER:$USER" "$WRAPPER_SCRIPT_PATH_STOP"
 
 # Create the systemd service file
+cat <<EOF > "$SERVICE_FILE"
+[Unit]
+Description=$SERVICE_NAME
+After=network.target
 
+[Service]
+User=$RUN_USER
+Group=$RUN_USER
+
+WorkingDirectory=$INSTALL_DIR
+ExecStart=$WRAPPER_SCRIPT_PATH_START
+# ExecStop=$WRAPPER_SCRIPT_PATH_STOP
+
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 # Enable and start the service
+systemctl daemon-reload
+systemctl enable "$SERVICE_NAME.service"
+systemctl start "$SERVICE_NAME.service"
+
