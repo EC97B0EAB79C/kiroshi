ee7ea33922550ce20ccd4492c77f1ba280c6e226
EC97B0EAB79C <19406885+EC97B0EAB79C@users.noreply.github.com>
2025-08-24 12:35:37 +0900

    Bug Fix

--- src/panels/picture_time_panel.py
+++ src/panels/picture_time_panel.py
@@ -5,11 +5,11 @@ from src.panels.time_panel import TimePanel
 from src.panels.picture_panel import PicturePanel
 
 
-class PictureTimePanel(PicturePanel):
+class PictureTimePanel(TimePanel):
     def __init__(self, width=800, height=480, settings={}, DEBUG=False):
         super().__init__(width, height, settings, DEBUG)
-        self.time_panel = TimePanel(width, height, settings, DEBUG)
+        self.picture_panel = PicturePanel(width, height, settings, DEBUG)
 
     def _draw(self, image):
-        self.time_panel._draw(image)
+        self.picture_panel._draw(image)
         return super()._draw(image)
