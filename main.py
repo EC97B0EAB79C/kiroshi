75f85f96aa6df5feb9cf1d88eab5bc612dcb45af
EC97B0EAB79C <EC97B0EAB79C@proton.me>
2026-03-19 17:02:49 +0900

    Application Class

--- main.py
+++ main.py
@@ -32,49 +32,8 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-def set_epd(name):
-    global epd, logger
-
-
-
-
-
 def main(settings_file):
-    global epd
-    logger.info(f"Starting application")
-    settings = Setting(settings_file)
-    set_epd(settings.get_epd_name())
-    settings.set_epd_settings(epd)
-    panels = {}
-
-    last_update = datetime.min
-    last_panel_id = None
-    duration = 0
-    while True:
-        FULL_REFRESH = False
-        now = datetime.now()
-        if (now - last_update).total_seconds() > duration:
-            panel_id, current_panel_spec, duration = settings.get_next_panel()
-            logger.info(f"Displaying panel {panel_id} for {duration} seconds")
-            last_update = now
-            FULL_REFRESH = True
-            # if panel_id != last_panel_id:
-            #   last_panel_id = panel_id
-            if current_panel_spec.get("refresh", False):
-                refresh_interval = settings.get_refresh_interval()
-            else:
-                refresh_interval = duration
-
-        if panel_id not in panels:
-            panels[panel_id] = load_panel(current_panel_spec, DEBUG=DEBUG)
-
-        image = panels[panel_id].draw()
-        if panels[panel_id].needs_refresh() or FULL_REFRESH:
-            set_panel(image, FULL_REFRESH=FULL_REFRESH)
-        else:
-            logger.debug("Image unchanged, skipping update")
-
-        sleep(refresh_interval)
+    pass
 
 
 if __name__ == "__main__":
